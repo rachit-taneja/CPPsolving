15e8fffc51fecaca168afe7bec37f4a8f84149f8
RACHIT TANEJA <tanejarachit111@gmail.com>
2026-06-10 01:15:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (77.66%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.size()==0)return 0;
+
+        int n=matrix.size();
+        int m=matrix[0].size();
+
+
+        int start=0;
+        int end=(n*m)-1;
+
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+
+            if(matrix[mid/m][mid%m]==target)
+            {
+                return true;
+            }
+            if(matrix[mid/m][mid%m]<target)
+            {
+                start=mid+1;
+                
+            }
+            else
+            {
+                end=mid-1;
+            }
+        }
+        return false;
+    }
+};
